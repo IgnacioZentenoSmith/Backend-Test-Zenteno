a15505296853528007bfd46997fdab835bd04944
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-12-22 00:31:42 -0300

    modelos de meals

--- backendzenteno/applications/meals/models.py
+++ backendzenteno/applications/meals/models.py
@@ -1,17 +1,53 @@
 from django.db import models
 
+from model_utils.models import TimeStampedModel
+
+from .managers import MealManager, CourseManager
+
 # Create your models here.
-class Meal(models.Model):
+class Meal(TimeStampedModel):
     # Constants
     # Fields
     meal_name = models.CharField(max_length=150)
 
     # Manager
+    objects = MealManager()
+
     # Standard methods
     def __str__(self):
 	    return self.meal_name
 
-class Meal_courses(models.Model):
+class Course(TimeStampedModel):
+    # Constants
+    ENTRADA = 'EN'
+    PLATO_PRINCIPAL = 'PP'
+    POSTRE = 'PO'
+    COURSE_TYPE_CHOICE = [
+        (ENTRADA, 'Entrada'),
+        (PLATO_PRINCIPAL, 'Plato principal'),
+        (POSTRE, 'Postre'),
+
+    ]
+
+    # Fields
+    course_name = models.CharField(max_length=50)
+    course_type = models.CharField(
+        max_length=2,
+        choices=COURSE_TYPE_CHOICE
+    )
+
+    # Manager
+    objects = CourseManager()
+
+    #Meta
+    class Meta:
+        unique_together = ['course_name', 'course_type']
+
+    # Standard methods
+    def __str__(self):
+	    return self.course_name + self.course_type
+
+class Meal_courses(TimeStampedModel):
     # Constants
     # Fields
     meal_id = models.ForeignKey(Meal, on_delete=models.CASCADE)
@@ -21,29 +57,8 @@ class Meal_courses(models.Model):
 
     #Meta
     class Meta:
-        unique_together = ['meal_id', 'course_id']
+        unique_together = ('meal_id', 'course_id')
+
     # Standard methods
     def __str__(self):
 	    return 'id meal: ' + str(self.meal_id) + '; id course: ' + str(self.course_id)
-
-class Course(models.Model):
-    # Constants
-    COURSE_TYPE_CHOICE = (
-      	(1, 'Entrada'),
-      	(2, 'Plato principal'),
-        (3, 'Postre'),
-    )
-
-    # Fields
-    course_name = models.CharField(max_length=50)
-    course_type = models.PositiveSmallIntegerField(choices=COURSE_TYPE_CHOICE)
-
-    # Manager
-
-    #Meta
-    class Meta:
-        unique_together = ['course_name', 'course_type']
-
-    # Standard methods
-    def __str__(self):
-	    return self.course_name + str(self.course_type)
