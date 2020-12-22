872f816a3781b576212596634075fa234b151a1d
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-12-21 21:07:05 -0300

    modelos de la app menu

--- backendzenteno/applications/menus/models.py
+++ backendzenteno/applications/menus/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    # Constants
+    # Fields
+    menu_date = models.DateField()
+
+    # Manager
+    # Standard methods
+    def __str__(self):
+	    return 'Menu de fecha: ' + self.menu_date
+
+class Menu_meal(models.Model):
+    # Constants
+    # Fields
+    menu_id = models.ForeignKey()
+    meal_id = models.ForeignKey()
+
+    # Manager
+    # Standard methods
+    def __str__(self):
+	    return 'id menu: ' + str(self.menu_id) + '; id meal: ' + str(self.meal_id)
