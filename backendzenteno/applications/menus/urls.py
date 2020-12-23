f03e615ff74f957b34518d9bce42466333c4413f
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-12-23 14:00:05 -0300

    urls de la aplicacion menus

--- backendzenteno/applications/menus/urls.py
+++ backendzenteno/applications/menus/urls.py
@@ -4,5 +4,31 @@ from . import views
 app_name = "menus_app"
 
 urlpatterns = [
+    # CRUD urls menu
+    path(
+        'menus/menu', 
+        views.MenuListView.as_view(),
+        name='menu-list',
+    ),
+    path(
+        'menus/menu_create', 
+        views.MenuCreateView.as_view(),
+        name='menu-create',
+    ),
+    path(
+        'menus/menu_edit/<pk>', 
+        views.MenuUpdateView.as_view(),
+        name='menu-edit',
+    ),
+    path(
+        'menus/menu_delete/<pk>', 
+        views.MenuDeleteView.as_view(),
+        name='menu-delete',
+    ),
+    path(
+        'menus/menu_detail/<pk>', 
+        views.MenuDetailView.as_view(),
+        name='menu-detail',
+    ),
 
 ]
