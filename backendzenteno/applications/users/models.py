7ac5936dc0817a5f37a5eaa377ec51681855a2f8
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-12-23 23:54:06 -0300

    crear auth token al registrar usuario

--- backendzenteno/applications/users/models.py
+++ backendzenteno/applications/users/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.urls import reverse
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 from .managers import UserManager
 
 # Create your models here.
@@ -28,3 +33,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 	# Standard methods
 	def __str__(self):
 		return self.user_name + self.user_email
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+	if created:
+		Token.objects.create(user=instance)
